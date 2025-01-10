69d44d0f0b6305c1e974ad548f804df6ceb7778b
miftahhuda9 <93971659+miftahhuda9@users.noreply.github.com>
2025-01-10 12:50:01 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CartController;
 
 Route::resource('posts', PostController::class);
 
@@ -9,10 +8,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/cart/item', [CartController::class]);
-Route::post('/cart/items', [CartController::class]);
-Route::get('/cart/item/{id}', [CartController::class]);
-Route::get('/cart/items', [CartController::class]);
-Route::delete('/cart/item/{id}', [CartController::class]);
-Route::delete('/cart/items', [CartController::class]);
-Route::put('/cart/item/{id}', [CartController::class});
